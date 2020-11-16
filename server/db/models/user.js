c64f8a6cc3c590f1088aac7cb709b6caa6a49e2a
Peter Boustani <boustanip718@gmail.com>
2020-11-16 11:25:15 -0500

    Have the new seed of over 100 instances in the database, however have one last issue with cycling through every order

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -5,28 +5,28 @@ const db = require('../db')
 const User = db.define('user', {
   // For Google Sign-in
   firstName: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
   },
   lastName: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
   },
   fullName: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
   },
   userName: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
   },
   email: {
     type: Sequelize.STRING,
     allowNull: false,
-    unique: true,
+    // unique: true,
     validate: {
       notEmpty: true,
-      isEmail: true
-    }
+      isEmail: true,
+    },
   },
   phoneNumber: {
-    type: Sequelize.INTEGER
+    type: Sequelize.INTEGER,
   },
   password: {
     type: Sequelize.STRING,
@@ -34,7 +34,7 @@ const User = db.define('user', {
     // This is a hack to get around Sequelize's lack of a "private" option.
     get() {
       return () => this.getDataValue('password')
-    }
+    },
   },
   salt: {
     type: Sequelize.STRING,
@@ -42,16 +42,16 @@ const User = db.define('user', {
     // This is a hack to get around Sequelize's lack of a "private" option.
     get() {
       return () => this.getDataValue('salt')
-    }
+    },
   },
   googleId: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
   },
   imageUrl: {
     type: Sequelize.STRING,
     defaultValue:
-      'https://i.pinimg.com/originals/f1/01/40/f101408a505673ae915f3afcc6588183.png'
-  }
+      'https://i.pinimg.com/originals/f1/01/40/f101408a505673ae915f3afcc6588183.png',
+  },
 })
 
 module.exports = User
@@ -59,18 +59,18 @@ module.exports = User
 /**
  * instanceMethods
  */
-User.prototype.correctPassword = function(candidatePwd) {
+User.prototype.correctPassword = function (candidatePwd) {
   return User.encryptPassword(candidatePwd, this.salt()) === this.password()
 }
 
 /**
  * classMethods
  */
-User.generateSalt = function() {
+User.generateSalt = function () {
   return crypto.randomBytes(16).toString('base64')
 }
 
-User.encryptPassword = function(plainText, salt) {
+User.encryptPassword = function (plainText, salt) {
   return crypto
     .createHash('RSA-SHA256')
     .update(plainText)
@@ -81,7 +81,7 @@ User.encryptPassword = function(plainText, salt) {
 /**
  * hooks
  */
-const setSaltAndPassword = user => {
+const setSaltAndPassword = (user) => {
   if (user.changed('password')) {
     user.salt = User.generateSalt()
     user.password = User.encryptPassword(user.password(), user.salt())
@@ -90,6 +90,6 @@ const setSaltAndPassword = user => {
 
 User.beforeCreate(setSaltAndPassword)
 User.beforeUpdate(setSaltAndPassword)
-User.beforeBulkCreate(users => {
+User.beforeBulkCreate((users) => {
   users.forEach(setSaltAndPassword)
 })
