c5016e79266b0f3be12c2900d091023d57701c55
Zachary Ernst <zernst3@live.com>
2020-11-17 21:40:40 -0500

    Added styling for the single Album view, cart and checkout.  Fixed cart number bug adding strings together.

--- client/components/cart-item.js
+++ client/components/cart-item.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import './Album.css'
+import './Button.css'
+import './Cart.css'
 
 const CartItem = ({
   id,
@@ -9,47 +12,48 @@ const CartItem = ({
   price,
   quantity,
   removeItem,
-  updateQuantity
+  updateQuantity,
 }) => {
   return (
-    <div>
-      <h3>
-        <Link to={`/album/${id}`}>
-          {title} - {artistName}
-        </Link>
-      </h3>
+    <div className="cartItem">
       <img src={imageUrl} />
-      <h3>
-        Price: ${(price / 100).toFixed(2)} - Total: $
-        {(price * quantity / 100).toFixed(2)}
-      </h3>
-      <h3>Quantity:</h3>
+      <div className="info">
+        <Link to={`/album/${id}`}>
+          <h1>{title}</h1>
+          <h2>{artistName}</h2>
+        </Link>
+        <div className="itemPricing">
+          <p>Unit Price: ${(price / 100).toFixed(2)}</p>
+          <p>Quantity:</p>
 
-      <select
-        onChange={evt => {
-          updateQuantity(id, evt.target.value)
-        }}
-      >
-        <option value={quantity}>Current Quantity: {quantity}</option>
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-        <option value="6">6</option>
-        <option value="7">7</option>
-        <option value="8">8</option>
-        <option value="9">9</option>
-        <option value="10">10</option>
-      </select>
-      <button
-        type="submit"
-        onClick={() => {
-          removeItem(id)
-        }}
-      >
-        Delete
-      </button>
+          <select
+            onChange={(evt) => {
+              updateQuantity(id, evt.target.value)
+            }}
+          >
+            <option value={quantity}>Current Quantity: {quantity}</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </select>
+          <p>Total: ${((price * quantity) / 100).toFixed(2)}</p>
+          <button
+            type="submit"
+            onClick={() => {
+              removeItem(id)
+            }}
+          >
+            Delete
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
